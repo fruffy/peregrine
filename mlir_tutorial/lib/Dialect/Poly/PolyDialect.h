55a6888e2c693b7943ee1d0043ffe399f602ed16
Vivi.W <76930199+vvbae@users.noreply.github.com>
2023-10-06 10:27:19 -0400

    Tutorial (#1)
    * tcpdump qa
    
    * midterm
    
    * DSL
    
    * BPF questions
    
    * tutorial
    
    * Delete midterm.md
    
    * Delete questions_tcpdump_10.md
    
    * Delete eBPF.md
    
    * Delete dsl.md
    
    * remove irrelevant
    
    * rm
    
    * rm ds_store & examples

---
+++ mlir_tutorial/lib/Dialect/Poly/PolyDialect.h
@@ -0,0 +1,9 @@
+#ifndef LIB_DIALECT_POLY_POLYDIALECT_H_
+#define LIB_DIALECT_POLY_POLYDIALECT_H_
+
+#include "mlir/include/mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyDialect.h.inc"
+
+#endif // LIB_DIALECT_POLY_POLYDIALECT_H_
