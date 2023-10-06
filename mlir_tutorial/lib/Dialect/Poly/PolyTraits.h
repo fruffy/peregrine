29711dd29a67b00793e85b2476f78e3b92263973
Vivi.W <polarsatellitest@gmail.com>
2023-10-06 09:37:57 -0400

    tutorial

---
+++ mlir_tutorial/lib/Dialect/Poly/PolyTraits.h
@@ -0,0 +1,30 @@
+#ifndef LIB_DIALECT_POLY_POLYTRAITS_H_
+#define LIB_DIALECT_POLY_POLYTRAITS_H_
+
+#include "mlir/include/mlir/IR/OpDefinition.h"
+
+namespace mlir::tutorial::poly
+{
+    template <typename ConcreteType>
+    class Has32BitArguments : public OpTrait::TraitBase<ConcreteType, Has32BitArguments>
+    {
+    public:
+        static LogicalResult verifyTrait(Operation *op)
+        {
+            for (auto type : op->getOperandTypes())
+            {
+                if (!type.isIntOrIndex())
+                    continue;
+
+                if (!type.isInteger(32))
+                {
+                    return op->emitOpError() << "requires each numeric operand to be a 32-bit integer";
+                }
+            }
+
+            return success();
+        }
+    };
+}
+
+#endif // LIB_DIALECT_POLY_POLYTRAITS_H_
---
+++ mlir_tutorial/lib/Dialect/Poly/PolyTypes.h
@@ -0,0 +1,9 @@
+#ifndef LIB_TYPES_POLY_POLYTYPES_H_
+#define LIB_TYPES_POLY_POLYTYPES_H_
+
+#include "mlir/include/mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.h.inc"
+
+#endif // LIB_TYPES_POLY_POLYTYPES_H_
